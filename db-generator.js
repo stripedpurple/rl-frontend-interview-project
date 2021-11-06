02189e4fab7d2812bab5ee44e8ed3a87dca72656
Austin Barrett <austin@stripedpurple.io>
2021-11-06 11:10:02 -0400

    updated db scema

--- db-generator.js
+++ db-generator.js
@@ -2,8 +2,9 @@ module.exports = () => {
     const {commerce: {productName, price}} = require('faker')
     
     return {
-        transactions: [...Array(20)].map(() => {
+        transactions: [...Array(20).keys()].map((idx) => {
             return {
+                id: idx,
                 description: productName(),
                 expense: (price() * (Math.random() < 0.5 ? -1 : 1)).toFixed(2)
             }
